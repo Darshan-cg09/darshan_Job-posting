a3bafcc36300e8070ea210b7dee9b6db24666636
Darshan-cg09 <darshancg34@gmail.com>
2025-06-20 21:04:44 -0700

    updated

--- backend/server.js
+++ backend/server.js
@@ -119,5 +119,5 @@ app.post('/api/jobs', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://13.60.46.121:${port}`);
+    console.log(`Server running at http://13.48.192.127:${port}`);
 });
